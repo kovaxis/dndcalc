4b5844b066f03641d97289af98fdf942d84ab568
kovaxis <martin.andrighetti@gmail.com>
2025-05-04 22:30:53 -0400

    fix base directory in deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), mdPlugin({ mode: [Mode.HTML] })],
+  base: process.env.BASE,
 })
